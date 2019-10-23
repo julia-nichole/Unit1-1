24fe25b3a2a7d239738b4d96fc97e78ab6a57664
julia-nichole <juliagalan191994@icloud.com>
2019-10-23 10:33:37 -0500

    const

---
+++ game.js
@@ -0,0 +1,16 @@
+// const//
+const story = {}
+
+const selected = {}
+
+
+
+// cached elements :
+const msg = document.getElementById('game');
+const button1 = document.getElementById('btn1');
+const button2= document.getElementById('btn2');
+
+
+// event listeners//
+document.getElementById('btn1').addEventListener('click', story1);
+document.getElementById('btn2').addEventListener('click', story2);
