ee336e9704b82ae40e69d4d43dbcf70751691092
julia-nichole <juliagalan191994@icloud.com>
2019-11-05 12:46:26 -0600

    fixes render function

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 // const//
 const story = {
- loadingScreen:"“Walls have ears. Doors have eyes. Trees have voices. Beasts tell lies. Beware the rain. Beware the snow. Beware the man You think you know.",
+ onLoad:"Walls have ears. Doors have eyes. Trees have voices. Beasts tell lies. Beware the rain. Beware the snow. Beware the man You think you know.",
+ 
  intro: "You and your partner our on your way to visit your mother, normally you arrive before dinner. But you had a late start and its now dark. So you decide to find a place for the night. You see a house in the distance . ..",
  
  rooms:"An elderly couple answers the door and informs you they don't rent rooms. But they would be happy to let you stay the night ",
@@ -12,23 +13,28 @@ const story = {
  dontStay1:" after getting back in the car and driving for a bit it begins to rain heavily making it difficult to see whats in front of you . ",
  
  sideRoad :"You pull off into the side road but the truck still follows it has sped up and is right behind you. You try to swerve but the back roads are small and you crash into the ditch. As you begin to lose consciousness the last thing you see is a shadowy figure dragging your partner from the crash you can hear their desperate screams for help but its to late and the world goes black .. ",
+ 
  rainPull:"After about 15 min the rain dies down and you are able to get to the next exit and find a motel to stay in for the night.  ",
+ 
  rainContin:"", 
 
  notEat:"You notice the elderly couple strangely watching your partner as if they are waiting for something...Suddenly your partner starts foaming at the mouth, choking on their own saliva. You rush to their side desperately trying to figure out how to help them in your own panic you don't realise the old couple closing in on you and the old man bashes you in the back of the head with a lamp. "
 }
 
 const selected = {
-    loadingScreens:{buttonOne:"choose",buttonTwo:"wisely"},
-    intro1:{buttonOne: "stop to see if they rent rooms", buttonTwo:"continue driving",},
-    rooms1:{buttonOne:" You decide to take the nice couple up on their offer and they welcome you inside ",buttonTwo:"The couple gives you major creeper vibes. So you politely decline their offer and high tail it back to the car ",},
-    dontStay2:{buttonOne:" pull over and wait for the rain to calm down " , buttontwo:" continue driving it just rain and you want to get to a hotel soon"},
+    onLoad:{"btn1":"choose","btn2":"wisely"},
+    
+    intro:{"btn1": "stop to see if they rent rooms", "btn2":"continue driving",},
+    
+    rooms1:{"btn1":" You decide to take the nice couple up on their offer and they welcome you inside ","btn2":"The couple gives you major creeper vibes. So you politely decline their offer and high tail it back to the car ",},
+    
+    dontStay2:{"btn1":" pull over and wait for the rain to calm down " , "btn2":" continue driving it just rain and you want to get to a hotel soon"},
   
    
-    driving1:{buttonOne:"Paranoia sets in so you decide tp pull off onto the next side road to see if the truck follows. ", buttonTwo:" You decide to speed up the car but continue in search of the nearest lodging "},
+    driving1:{"btn1":"Paranoia sets in so you decide tp pull off onto the next side road to see if the truck follows. ", "btn2":" You decide to speed up the car but continue in search of the nearest lodging "},
     
     
-    eatOrNot:{buttonOne:"You decline any refreshments but your partner eagerly takes some. ",buttonTwo:"You and your partner graciously accept you haven't eaten since the morning and are starving."}
+    eatOrNot:{"btn1":"You decline any refreshments but your partner eagerly takes some. ","btn2":"You and your partner graciously accept you haven't eaten since the morning and are starving."}
  
 }
 
@@ -50,15 +56,15 @@ document.getElementById("btn2").addEventListener("click", storyTwo);
 document.getElementById("start").addEventListener("click", startStory);
 document.getElementById("reset").addEventListener("click", resetGame);
 
-
-// functions
 var state;
+// functions
+
 init();
 function startStory(evt){ 
 switch (state){
-    case story.loadingScreen:
-        state = story.loadingScreen
-        break;
+    case story.onLoad:
+        state = story.intro;
+      break;
     default:
 }
 render();
@@ -66,19 +72,22 @@ render();
 
 function storyOne(){
     switch (state) {
-        case story.loadingScreen:
+        case story.onLoad:
           state = story.intro;
           break;
-        case story.rooms:
-          state = story.rooms22;
+        case story.intro:
+          state = story.rooms;
+        case story.rooms1:
+        state = story.rooms1;
           break;
     default:
 }}
 function storyTwo(){
 switch (state){
-  case story.loadingScreen:
+  case story.onLoad:
     state = story.intro;
     break;
+  default:  
 }
 }
 function resetGame(evt) {
@@ -88,18 +97,24 @@ function resetGame(evt) {
 
   function render() {
     sectionEl.textContent = state;
-    startStory();
+    stateSwitch();
   }
   function init(){
-      state = story.loadingScreen;
+      state = story.onLoad;
       render();
   }
 
   function stateSwitch(){
     switch(state){
-      case story.loadingScreen:
-        button1.textContent = selected.loadingScreen["btn1"];
-        button2.textContent = selected.loadingScreen["btn2"];
+      case story.onLoad:
+        button1.textContent = selected.onLoad["btn1"];
+        button2.textContent = selected.onLoad["btn2"];
         break;
+      case story.intro:
+        button1.textContent = selected.intro["btn1"];
+        button2.textContent = selected.intro["btn2"];
+        break;
+        default:
+
     }
   }
